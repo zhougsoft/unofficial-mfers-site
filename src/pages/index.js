0974438b401e80a9464e7f2c73003964cb9ec327
Akshay Kashyap <akshay.kashyap95@gmail.com>
2022-02-08 14:40:40 -0500

    fix spacing

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
 import * as React from 'react'
 import Layout from '../components/Layout'
 import ExtLink from '../components/ExtLink'
-import sartoshiImg from '../assets/images/sartoshi.png'
 
+import sartoshiImg from '../assets/images/sartoshi.png'
 import openseaLogoImg from '../assets/images/os.png'
 import svgDiscordLogo from '../assets/svg/discord-logo.svg'
 import svgEtherscanLogo from '../assets/svg/etherscan-logo.svg'
