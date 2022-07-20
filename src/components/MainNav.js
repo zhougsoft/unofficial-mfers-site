ff18f395118ef97db4a0ea9fb89c6dff13100d0f
anonchickenlegs <anonchickenlegs@gmail.com>
2022-07-20 14:15:59 -0400

    add bootstrap and initial commit

--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -1,32 +1,58 @@
 import React from 'react'
-import { Link } from 'gatsby'
-import Container from '../components/Container'
-
+import { Navbar, Container, Nav } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '../styles/main.scss'
+import {
+	DISCORD_LINK,
+	OPENSEA_LINK,
+	LOOKSRARE_LINK,
+	ETHERSCAN_LINK,
+	TWITTER_LINK,
+} from '../constants'
 const MainNav = () => {
 	return (
-		<nav className="main-nav">
-			<Container>
-				<div className="nav-content">
-					<div className="nav-brand">
-						<Link to="/">unofficial mfers</Link>
-					</div>
-					<ul>
-						<li>
-							<Link to="/">home</Link>
-						</li>
-						<li>
-							<Link to="/governance">governance</Link>
-						</li>
-						<li>
-							<Link to="/links">links</Link>
-						</li>
-						<li>
-							<Link to="/links">meme craftor (coming soon)</Link>
-						</li>
-					</ul>
-				</div>
-			</Container>
-		</nav>
+		<>
+			<Navbar expand="sm" className="nav-bar-background-color new-nav">
+				<Container>
+					<Navbar.Brand>
+						<img src="/head-base-smokeless.png" width="45" height="45" />
+					</Navbar.Brand>
+					<Navbar.Toggle aria-controls="basic-navbar-nav" />
+					<Navbar.Collapse id="basic-navbar-nav">
+						<Nav className="me-auto">
+							<Nav.Link
+								href="https://twitter.com/chickenswhocode"
+								target="_blank"
+								className="main-nav-text-color"
+							>
+								mfer hub
+							</Nav.Link>
+							<Nav.Link
+								href="https://discord.gg/sbBvgAQz8r"
+								target="_blank"
+								className="main-nav-text-color"
+							>
+								mfer governance
+							</Nav.Link>
+						</Nav>
+						<Nav id="basic-navbar-nav">
+							<Nav.Link href="#connect-wallet" className="text-color">
+								<img src="/discord_icon.png" />
+							</Nav.Link>
+							<Nav.Link href="#connect-wallet" className="text-color">
+								<img src="/twitter_icon.png" />
+							</Nav.Link>
+							<Nav.Link href="#connect-wallet" className="text-color">
+								<img src="/twitter_icon.png" />
+							</Nav.Link>
+							<Nav.Link href="#connect-wallet" className="text-color">
+								<img src="/twitter_icon.png" />
+							</Nav.Link>
+						</Nav>
+					</Navbar.Collapse>
+				</Container>
+			</Navbar>
+		</>
 	)
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,56 +1,203 @@
 import * as React from 'react'
 import Layout from '../components/Layout'
-import ExtLink from '../components/ExtLink'
-
-import {
-	DISCORD_LINK,
-	OPENSEA_LINK,
-	LOOKSRARE_LINK,
-	ETHERSCAN_LINK,
-	TWITTER_LINK,
-} from '../constants'
-
-import sartoshiImg from '../assets/images/sartoshi.png'
-import svgDiscordLogo from '../assets/svg/discord-logo.svg'
-import svgOpenSeaLogo from '../assets/svg/os-logo.svg'
-import svgLooksRareLogo from '../assets/svg/looksrare-logo.svg'
-import svgEtherscanLogo from '../assets/svg/etherscan-logo.svg'
-import svgTwitterLogo from '../assets/svg/twitter-logo.svg'
-
+import { Card } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
 const IndexPage = () => {
 	return (
 		<Layout pageTitle="unofficial mfers">
 			<section className="home-content">
 				<div className="home-title">
-					<h1>unofficial mfers</h1>
+					<h1>gm mfers "sun"</h1>
 					<p>
-						the <strong>officially unofficial</strong> website of the{' '}
-						<strong>mfers</strong>
+						you can state a roadmap that says where you will go, but you can
+						also plant seeds and see where they grow
 					</p>
 				</div>
-				<div className="home-links">
-					<ExtLink href={DISCORD_LINK}>
-						<img src={svgDiscordLogo} alt="Discord logo icon" />
-					</ExtLink>
-					<ExtLink href={OPENSEA_LINK}>
-						<img src={svgOpenSeaLogo} alt="Opensea logo icon" />
-					</ExtLink>
-					<ExtLink href={LOOKSRARE_LINK}>
-						<img src={svgLooksRareLogo} alt="LooksRare logo icon" />
-					</ExtLink>
-					<ExtLink href={ETHERSCAN_LINK}>
-						<img src={svgEtherscanLogo} alt="Etherscan logo icon" />
-					</ExtLink>
-					<ExtLink href={TWITTER_LINK}>
-						<img src={svgTwitterLogo} alt="Twitter logo icon" />
-					</ExtLink>
+				<Card className="d-flex">
+					{' '}
+					<Card.Body className="card-props">
+						What are mfers? <img src="/go_to_icon.png" width="16" />
+					</Card.Body>
+				</Card>
+			</section>
+			<section>
+				<div class="slider">
+					<div class="slide-track">
+						<div class="slide">
+							<img
+								src="https://img.seadn.io/files/184c2ec7255be9c249f873d1a83b0792.png?fit=max&w=600"
+								alt=""
+							/>
+						</div>
+						<div class="slide">
+							<img
+								src="https://img.seadn.io/files/184c2ec7255be9c249f873d1a83b0792.png?fit=max&w=600"
+								alt=""
+							/>
+						</div>{' '}
+						<div class="slide">
+							<img
+								src="https://img.seadn.io/files/184c2ec7255be9c249f873d1a83b0792.png?fit=max&w=600"
+								alt=""
+							/>
+						</div>{' '}
+						<div class="slide">
+							<img
+								src="https://img.seadn.io/files/184c2ec7255be9c249f873d1a83b0792.png?fit=max&w=600"
+								alt=""
+							/>
+						</div>{' '}
+						<div class="slide">
+							<img
+								src="https://img.seadn.io/files/184c2ec7255be9c249f873d1a83b0792.png?fit=max&w=600"
+								alt=""
+							/>
+						</div>{' '}
+						<div class="slide">
+							<img
+								src="https://img.seadn.io/files/184c2ec7255be9c249f873d1a83b0792.png?fit=max&w=600"
+								alt=""
+							/>
+						</div>{' '}
+						<div class="slide">
+							<img
+								src="https://img.seadn.io/files/184c2ec7255be9c249f873d1a83b0792.png?fit=max&w=600"
+								alt=""
+							/>
+						</div>{' '}
+						<div class="slide">
+							<img
+								src="https://img.seadn.io/files/184c2ec7255be9c249f873d1a83b0792.png?fit=max&w=600"
+								alt=""
+							/>
+						</div>{' '}
+						<div class="slide">
+							<img
+								src="https://img.seadn.io/files/184c2ec7255be9c249f873d1a83b0792.png?fit=max&w=600"
+								alt=""
+							/>
+						</div>{' '}
+						<div class="slide">
+							<img
+								src="https://img.seadn.io/files/184c2ec7255be9c249f873d1a83b0792.png?fit=max&w=600"
+								alt=""
+							/>
+						</div>{' '}
+						<div class="slide">
+							<img
+								src="https://img.seadn.io/files/184c2ec7255be9c249f873d1a83b0792.png?fit=max&w=600"
+								alt=""
+							/>
+						</div>{' '}
+						<div class="slide">
+							<img
+								src="https://img.seadn.io/files/184c2ec7255be9c249f873d1a83b0792.png?fit=max&w=600"
+								alt=""
+							/>
+						</div>{' '}
+						<div class="slide">
+							<img
+								src="https://img.seadn.io/files/184c2ec7255be9c249f873d1a83b0792.png?fit=max&w=600"
+								alt=""
+							/>
+						</div>{' '}
+						<div class="slide">
+							<img
+								src="https://img.seadn.io/files/184c2ec7255be9c249f873d1a83b0792.png?fit=max&w=600"
+								alt=""
+							/>
+						</div>{' '}
+					</div>
+				</div>
+				<div class="slider">
+					<div class="slide-track-right">
+						<div class="slide">
+							<img
+								src="https://img.seadn.io/files/184c2ec7255be9c249f873d1a83b0792.png?fit=max&w=600"
+								alt=""
+							/>
+						</div>
+						<div class="slide">
+							<img
+								src="https://img.seadn.io/files/184c2ec7255be9c249f873d1a83b0792.png?fit=max&w=600"
+								alt=""
+							/>
+						</div>{' '}
+						<div class="slide">
+							<img
+								src="https://img.seadn.io/files/184c2ec7255be9c249f873d1a83b0792.png?fit=max&w=600"
+								alt=""
+							/>
+						</div>{' '}
+						<div class="slide">
+							<img
+								src="https://img.seadn.io/files/184c2ec7255be9c249f873d1a83b0792.png?fit=max&w=600"
+								alt=""
+							/>
+						</div>{' '}
+						<div class="slide">
+							<img
+								src="https://img.seadn.io/files/184c2ec7255be9c249f873d1a83b0792.png?fit=max&w=600"
+								alt=""
+							/>
+						</div>{' '}
+						<div class="slide">
+							<img
+								src="https://img.seadn.io/files/184c2ec7255be9c249f873d1a83b0792.png?fit=max&w=600"
+								alt=""
+							/>
+						</div>{' '}
+						<div class="slide">
+							<img
+								src="https://img.seadn.io/files/184c2ec7255be9c249f873d1a83b0792.png?fit=max&w=600"
+								alt=""
+							/>
+						</div>{' '}
+						<div class="slide">
+							<img
+								src="https://img.seadn.io/files/184c2ec7255be9c249f873d1a83b0792.png?fit=max&w=600"
+								alt=""
+							/>
+						</div>{' '}
+						<div class="slide">
+							<img
+								src="https://img.seadn.io/files/184c2ec7255be9c249f873d1a83b0792.png?fit=max&w=600"
+								alt=""
+							/>
+						</div>{' '}
+						<div class="slide">
+							<img
+								src="https://img.seadn.io/files/184c2ec7255be9c249f873d1a83b0792.png?fit=max&w=600"
+								alt=""
+							/>
+						</div>{' '}
+						<div class="slide">
+							<img
+								src="https://img.seadn.io/files/184c2ec7255be9c249f873d1a83b0792.png?fit=max&w=600"
+								alt=""
+							/>
+						</div>{' '}
+						<div class="slide">
+							<img
+								src="https://img.seadn.io/files/184c2ec7255be9c249f873d1a83b0792.png?fit=max&w=600"
+								alt=""
+							/>
+						</div>{' '}
+						<div class="slide">
+							<img
+								src="https://img.seadn.io/files/184c2ec7255be9c249f873d1a83b0792.png?fit=max&w=600"
+								alt=""
+							/>
+						</div>{' '}
+						<div class="slide">
+							<img
+								src="https://img.seadn.io/files/184c2ec7255be9c249f873d1a83b0792.png?fit=max&w=600"
+								alt=""
+							/>
+						</div>{' '}
+					</div>
 				</div>
 			</section>
-			<img
-				src={sartoshiImg}
-				alt="sartoshi smiling and vibing with headphones in their chair, facing to the right"
-				className="mfer-pic"
-			/>
 		</Layout>
 	)
 }
