82d8cc4915ffefb65f33cf2bdff6260f5727a1db
zhoug <90252209+zhoug0x@users.noreply.github.com>
2022-08-02 21:25:21 -0300

    swapping internal nav links components to gatsby links, changed hardcoded links to constants, removing unused navbar styles

--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -7,66 +7,63 @@ import {
 	DISCORD_LINK,
 	OPENSEA_LINK,
 	LOOKSRARE_LINK,
-	ETHERSCAN_LINK,
 	TWITTER_LINK,
 } from '../constants'
 const MainNav = () => {
 	return (
-		<>
-			<Navbar expand="sm" className="nav-bar-background-color new-nav">
-				<Container>
-					<Navbar.Brand>
-						<Nav.Link href="/" className="main-nav-text-color">
-							<img src="/head-base-smokeless.png" width="45" height="45" />
+		<Navbar id="nav" expand="sm" className="nav-bar-background-color">
+			<Container>
+				<Navbar.Brand>
+					<Nav.Link href="/" className="main-nav-text-color">
+						<img src="/head-base-smokeless.png" width="45" height="45" />
+					</Nav.Link>
+				</Navbar.Brand>
+				<Navbar.Toggle aria-controls="basic-navbar-nav" />
+				<Navbar.Collapse id="basic-navbar-nav">
+					<Nav className="me-auto">
+						<Link to="/" className="internal-nav-link">
+							home
+						</Link>
+						<Link to="/governance" className="internal-nav-link">
+							governance
+						</Link>
+						<Link to="/links" className="internal-nav-link">
+							links
+						</Link>
+					</Nav>
+					<Nav className="icon-nav-bar" id="mx-3">
+						<Nav.Link
+							href={DISCORD_LINK}
+							className="text-color spacing"
+							target="_blank"
+						>
+							<img src="/Discord.svg" />
 						</Nav.Link>
-					</Navbar.Brand>
-					<Navbar.Toggle aria-controls="basic-navbar-nav" />
-					<Navbar.Collapse id="basic-navbar-nav">
-						<Nav className="me-auto">
-							<Nav.Link href="/" className="main-nav-text-color">
-								home
-							</Nav.Link>
-							<Nav.Link href="/governance" className="main-nav-text-color">
-								mfer governance
-							</Nav.Link>
-							<Nav.Link href="/links" className="main-nav-text-color">
-								links
-							</Nav.Link>
-						</Nav>
-						<Nav className="icon-nav-bar" id="mx-3">
-							<Nav.Link
-								href="https://discord.gg/9vWnDfUnN6"
-								className="text-color spacing"
-								target="_blank"
-							>
-								<img src="/Discord.svg" />
-							</Nav.Link>
-							<Nav.Link
-								href="https://twitter.com/unofficialmfers"
-								className="text-color spacing"
-								target="_blank"
-							>
-								<img src="/Twitter.svg" />
-							</Nav.Link>
-							<Nav.Link
-								href="https://opensea.io/collection/mfers"
-								className="text-color spacing"
-								target="_blank"
-							>
-								<img src="/Opensea.svg" />
-							</Nav.Link>
-							<Nav.Link
-								href="https://looksrare.org/collections/0x79FCDEF22feeD20eDDacbB2587640e45491b757f"
-								className="text-color spacing"
-								target="_blank"
-							>
-								<img src="/Looksrare.svg" />
-							</Nav.Link>
-						</Nav>
-					</Navbar.Collapse>
-				</Container>
-			</Navbar>
-		</>
+						<Nav.Link
+							href={TWITTER_LINK}
+							className="text-color spacing"
+							target="_blank"
+						>
+							<img src="/Twitter.svg" />
+						</Nav.Link>
+						<Nav.Link
+							href={OPENSEA_LINK}
+							className="text-color spacing"
+							target="_blank"
+						>
+							<img src="/Opensea.svg" />
+						</Nav.Link>
+						<Nav.Link
+							href={LOOKSRARE_LINK}
+							className="text-color spacing"
+							target="_blank"
+						>
+							<img src="/Looksrare.svg" />
+						</Nav.Link>
+					</Nav>
+				</Navbar.Collapse>
+			</Container>
+		</Navbar>
 	)
 }
 
